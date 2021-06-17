30f21964690b0a384a403d8787542ee0e5c2ecf5
Victor <adsvictor@outlook.com>
2021-06-17 12:43:33 -0300

    Learning directives

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,8 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 registerLocaleData(localePt);
 
@@ -45,6 +47,8 @@ registerLocaleData(localePt);
     ProductRead2Component,
     ProductUpdateComponent,
     ProductDeleteComponent,
+    RedDirective,
+    ForDirective,
   ],
   imports: [
     BrowserModule,
